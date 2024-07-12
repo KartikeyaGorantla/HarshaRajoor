963968464b9f619cc4495e8e6cb4fa0878a03879
GK <92967659+KartikeyaGorantla@users.noreply.github.com>
2024-07-13 03:05:10 +0530

    .

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,10 +3,10 @@ export const personalData = {
     profile: '/profile.jpg',
     designation: "Cyber Security Aspirant",
     description: "I am currently pursuing a B.Tech in Computer Science with a Cyber Security specialization course. I am proficient in Python and have worked with many libraries. I am sure I can solve problems of difficulty level 8/10. I am a CTF player with 2 years of experience. I have little experience with forensics and malware analysis. I have a special interest in digital forensics and malware analysis. I have always been focused on sharpening my skills in these specific domains, and I like prompting with AI and have created many websites and projects with that.",
-    email: 'harcharajoorethical@gmail.com',
+    email: 'harsharajoorethical@gmail.com',
     phone: '+91 8660481227',
     address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
     github: 'https://github.com/harsharajoor2517',
     linkedIn: 'https://www.linkedin.com/in/harsha-rajoor-b15b9122b',
-    resume: "https://drive.google.com/file/d/14R8d2jKx5miWnNPdPp1vn518h5jRbZEc/view?usp=sharing"
+    resume: "https://drive.google.com/file/d/1hUZONFtWJ2oDiGOebmCb0pJ2nx_lPckZ/view?usp=sharing"
 }
